44d8385672f4c59c78600e2cb1e8be043262220d
pesdesigner <pauloeduardo.dg@outlook.com>
2024-11-17 12:04:41 -0300

    GET game by id

--- src/main/java/com/graficoeweb/gamelist/services/GameService.java
+++ src/main/java/com/graficoeweb/gamelist/services/GameService.java
@@ -1,10 +1,13 @@
 package com.graficoeweb.gamelist.services;
 
+import com.graficoeweb.gamelist.dto.GameDTO;
 import com.graficoeweb.gamelist.dto.GameMinDTO;
 import com.graficoeweb.gamelist.entities.Game;
 import com.graficoeweb.gamelist.repositories.GameRepository;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -13,6 +16,12 @@ public class GameService {
 
     @Autowired
     private GameRepository gameRepository;
+
+    @Transactional(readOnly = true)
+    public GameDTO findById(Long id) {
+        Game result = gameRepository.findById(id).get();
+        return new GameDTO(result);
+    }
     public List<GameMinDTO> findAll() {
        List<Game> result = gameRepository.findAll();
        return result.stream().map(x -> new GameMinDTO(x)).toList();
